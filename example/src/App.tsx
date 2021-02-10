7ae87d35bc2c94917ab4c98a1c111709adb4d983
Atrius <atriusedm@gmail.com>
2021-02-10 03:44:49 -0500

    Export components and add badge to demo page

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-import { Progress } from 'react-progress-bars'
-import 'react-progress-bars/dist/index.css'
+import { ProgressBadge } from 'react-progress-bars'
 
 const App = () => {
-  return <Progress look="linear-gradient(to bottom right, orange 0%, purple 100%)" value="50%" />
+  return <ProgressBadge style={{
+    margin: '10em'
+  }} text="Hello world!"  double look="linear-gradient(to bottom right, orange 0%, purple 100%)" value="50%" />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 import { Progress } from './components/Progress'
+import { ProgressBadge } from './components/ProgressBadge'
 
-export { Progress }
+export { Progress, ProgressBadge }
