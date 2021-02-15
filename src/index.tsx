89275cb0baf2956f026b8124e6c0ae61ed32392e
Atrius <atriusedm@gmail.com>
2021-02-15 03:55:32 -0500

    Export RadialProgress

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { Progress } from './components/Progress'
 import { ProgressBadge } from './components/ProgressBadge'
 import { ExperienceBar } from './components/ExperienceBar'
+import { RadialProgress } from './components/RadialProgress'
 
-export { Progress, ProgressBadge, ExperienceBar }
+export { Progress, ProgressBadge, ExperienceBar, RadialProgress }
